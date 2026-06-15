ece897775343d57ec529a34200e91fce56c97f95
PranavD2004 <pranavdurge7604@gmail.com>
2026-06-15 17:35:04 +0530

    re-modified

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from jenkins, added webhook, we are from 18 May batch');
-  res.send('For my projects go to https://github.com/Pranav-Labs07');
+  res.send('Hiiiii from jenkins, added webhook, we are from 18 May batch');
 });
 
 app.listen(port, () => {
